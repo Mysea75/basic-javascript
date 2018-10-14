de478bd85bba1e8b927f2eef5ff16a595adcd45b
Ghulam Moinuddin <sea.moin@gmail.com>
2018-10-14 13:10:51 +0530

    Package.json and other development files modified

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 console.log("Hello World from javascript");
+
+// var moment = require('moment');
+import moment from 'moment';
+
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
-window.onload = function(){
+window.onload = ()=>{
     document.getElementById("currentDateTime").textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
 }
+
+console.log("Is changes Occurs");
+console.log("Is changed agian");
